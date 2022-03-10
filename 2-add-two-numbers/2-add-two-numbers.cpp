755df5959f3bf9883c5e008fa86a30677a734105
Rashid-Hussain-CSE <52508334+Rashid-Hussain-CSE@users.noreply.github.com>
2022-03-10 13:15:36 +0530

    Time: 58 ms (39.53%), Space: 71.6 MB (11.57%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -10,25 +10,14 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head){
-        ListNode *temp, *prev=NULL, *curr=head;
-        while(curr){
-            temp = curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=temp;
-        }
-        return prev;
-    }
+    
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *rl1 = (l1);
-        ListNode *rl2 = (l2);
         ListNode *result = new ListNode(0);
         ListNode *resultHead=result;
         int carry=0;
-        while(rl1 || rl2){
-            if(rl1 && rl2){
-                int sum = rl1->val + rl2->val + carry;
+        while(l1 || l2){
+            if(l1 && l2){
+                int sum = l1->val + l2->val + carry;
                 if(sum<10){
                     result->next = new ListNode(sum);
                     carry=0;
@@ -39,11 +28,11 @@ public:
                     result->next = new ListNode(sum%10);
                     result = result->next;
                 }
-                rl1=rl1->next;
-                rl2=rl2->next;
+                l1=l1->next;
+                l2=l2->next;
             }
-            else if(rl1){
-                int sum = rl1->val + carry;
+            else if(l1){
+                int sum = l1->val + carry;
                 if(sum<10){
                     result->next = new ListNode(sum);
                     carry=0;
@@ -54,10 +43,10 @@ public:
                     result->next = new ListNode(sum%10);
                     result = result->next;
                 }
-                rl1=rl1->next;
+                l1=l1->next;
             }
-            else if(rl2){
-                int sum = rl2->val + carry;
+            else if(l2){
+                int sum = l2->val + carry;
                 if(sum<10){
                     result->next = new ListNode(sum);
                     carry=0;
@@ -68,7 +57,7 @@ public:
                     result->next = new ListNode(sum%10);
                     result = result->next;
                 }
-                rl2=rl2->next;
+                l2=l2->next;
             }
         }
         if(carry!=0){
